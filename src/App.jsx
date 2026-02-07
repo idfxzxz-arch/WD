b9337b0b91cbf74f2bbec0aca46a968566f0bdca
idfxzxz-arch <idfxzxz@gmail.com>
2026-02-07 21:38:01 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero"
 import Section from "./components/Section"
 import About from "./components/About"
 import Works from "./components/works"
-import Cta from "./components/Cta"
+import CTA from "./components/CTA"
 import TeamHero from "./components/TeamHero"
 import Footer from "./components/Footer"
 import Scope from "./components/scope"
@@ -12,21 +12,13 @@ export default function App() {
   return (
     <>
       <Navbar />
-
       <Hero />
-
       <Section />
-
       <About />
-
       <Works />
-
       <Scope />
-
-      <Cta />
-
+      <CTA />
       <TeamHero />
-
       <Footer />
     </>
   )
